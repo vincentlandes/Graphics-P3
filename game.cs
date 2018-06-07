9ff1e9bed18a4cfe810cdb389656095dca8fbdf3
vincentlandes <vincentlandes@users.noreply.github.com>
2018-06-07 16:22:09 +0200

    eerste stapjes joe

---
+++ game.cs
@@ -0,0 +1,92 @@
+﻿using System.Diagnostics;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+// minimal OpenTK rendering framework for UU/INFOGR
+// Jacco Bikker, 2016
+
+namespace Template_P3 {
+
+class Game
+{
+	// member variables
+	public Surface screen;					// background surface for printing etc.
+	Mesh mesh, floor;						// a mesh to draw using OpenGL
+	const float PI = 3.1415926535f;			// PI
+	float a = 0;							// teapot rotation angle
+	Stopwatch timer;						// timer for measuring frame duration
+	Shader shader;							// shader to use for rendering
+	Shader postproc;						// shader to use for post processing
+	Texture wood;							// texture to use for rendering
+	RenderTarget target;					// intermediate render target
+	ScreenQuad quad;						// screen filling quad for post processing
+	bool useRenderTarget = true;
+
+	// initialize
+	public void Init()
+	{
+		// load teapot
+		mesh = new Mesh( "../../assets/teapot.obj" );
+		floor = new Mesh( "../../assets/floor.obj" );
+		// initialize stopwatch
+		timer = new Stopwatch();
+		timer.Reset();
+		timer.Start();
+		// create shaders
+		shader = new Shader( "../../shaders/vs.glsl", "../../shaders/fs.glsl" );
+		postproc = new Shader( "../../shaders/vs_post.glsl", "../../shaders/fs_post.glsl" );
+		// load a texture
+		wood = new Texture( "../../assets/wood.jpg" );
+		// create the render target
+		target = new RenderTarget( screen.width, screen.height );
+		quad = new ScreenQuad();
+	}
+
+	// tick for background surface
+	public void Tick()
+	{
+		screen.Clear( 0 );
+		screen.Print( "hello world", 2, 2, 0xffff00 );
+	}
+
+	// tick for OpenGL rendering code
+	public void RenderGL()
+	{
+		// measure frame duration
+		float frameDuration = timer.ElapsedMilliseconds;
+		timer.Reset();
+		timer.Start();
+	
+		// prepare matrix for vertex shader
+		Matrix4 transform = Matrix4.CreateFromAxisAngle( new Vector3( 0, 1, 0 ), a );
+		transform *= Matrix4.CreateTranslation( 0, -4, -15 );
+		transform *= Matrix4.CreatePerspectiveFieldOfView( 1.2f, 1.3f, .1f, 1000 );
+
+		// update rotation
+		a += 0.001f * frameDuration; 
+		if (a > 2 * PI) a -= 2 * PI;
+
+		if (useRenderTarget)
+		{
+			// enable render target
+			target.Bind();
+
+			// render scene to render target
+			mesh.Render( shader, transform, wood );
+			floor.Render( shader, transform, wood );
+
+			// render quad
+			target.Unbind();
+			quad.Render( postproc, target.GetTextureID() );
+		}
+		else
+		{
+			// render scene directly to the screen
+			mesh.Render( shader, transform, wood );
+			floor.Render( shader, transform, wood );
+		}
+	}
+}
+
+} // namespace Template_P3
---
+++ template.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Drawing;
+using System.Globalization;
+using System.Threading;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace Template_P3 {
+
+public class OpenTKApp : GameWindow
+{
+	static int screenID;
+	static Game game;
+	static bool terminated = false;
+	protected override void OnLoad( EventArgs e )
+	{
+		// called upon app init
+		GL.ClearColor( Color.Black );
+		GL.Enable( EnableCap.Texture2D );
+		GL.Disable( EnableCap.DepthTest );
+		GL.Hint( HintTarget.PerspectiveCorrectionHint, HintMode.Nicest );
+		ClientSize = new Size( 640, 400 );
+		game = new Game();
+		game.screen = new Surface( Width, Height );
+		Sprite.target = game.screen;
+		screenID = game.screen.GenTexture();
+		game.Init();
+	}
+	protected override void OnUnload( EventArgs e )
+	{
+		// called upon app close
+		GL.DeleteTextures( 1, ref screenID );
+		Environment.Exit( 0 ); // bypass wait for key on CTRL-F5
+	}
+	protected override void OnResize( EventArgs e )
+	{
+		// called upon window resize
+		GL.Viewport(0, 0, Width, Height);
+		GL.MatrixMode( MatrixMode.Projection );
+		GL.LoadIdentity();
+		GL.Ortho( -1.0, 1.0, -1.0, 1.0, 0.0, 4.0 );
+	}
+	protected override void OnUpdateFrame( FrameEventArgs e )
+	{
+		// called once per frame; app logic
+		var keyboard = OpenTK.Input.Keyboard.GetState();
+		if (keyboard[OpenTK.Input.Key.Escape]) this.Exit();
+	}
+	protected override void OnRenderFrame( FrameEventArgs e )
+	{
+		// called once per frame; render
+		game.Tick();
+		if (terminated) 
+		{
+			Exit();
+			return;
+		}
+		// set the state for rendering the quad
+		GL.ClearColor( Color.Black );
+		GL.Enable( EnableCap.Texture2D );
+		GL.Disable( EnableCap.DepthTest );
+		GL.Color3( 1.0f, 1.0f, 1.0f );
+		GL.BindTexture( TextureTarget.Texture2D, screenID );
+		GL.TexImage2D( TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, 
+						game.screen.width, game.screen.height, 0, 
+						OpenTK.Graphics.OpenGL.PixelFormat.Bgra, 
+						PixelType.UnsignedByte, game.screen.pixels 
+						);
+		// GL.Clear( ClearBufferMask.ColorBufferBit ); /* not needed */
+		GL.MatrixMode( MatrixMode.Modelview );
+		GL.LoadIdentity();
+		GL.MatrixMode( MatrixMode.Projection );
+		GL.LoadIdentity();
+		GL.Begin( PrimitiveType.Quads );
+		GL.TexCoord2( 0.0f, 1.0f ); GL.Vertex2( -1.0f, -1.0f );
+		GL.TexCoord2( 1.0f, 1.0f ); GL.Vertex2(  1.0f, -1.0f );
+		GL.TexCoord2( 1.0f, 0.0f ); GL.Vertex2(  1.0f,  1.0f );
+		GL.TexCoord2( 0.0f, 0.0f ); GL.Vertex2( -1.0f,  1.0f );
+		GL.End();
+		// prepare for generic OpenGL rendering
+		GL.Enable( EnableCap.DepthTest );
+		GL.Clear( ClearBufferMask.DepthBufferBit );
+		GL.Disable( EnableCap.Texture2D );
+		// do OpenGL rendering
+		game.RenderGL();
+		// swap buffers
+		SwapBuffers();
+	}
+	public static void Main( string[] args ) 
+	{ 
+		// entry point
+		Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo( "en-US" ); // thanks Mathijs
+		using (OpenTKApp app = new OpenTKApp()) { app.Run( 30.0, 0.0 ); }
+	}
+}
+
+} // namespace Template_P3
